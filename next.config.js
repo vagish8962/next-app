d2b63c683f5f52834d418f5771987a6403946bab
gupta, Vagish <vagishgupta22@gmail.com>
2023-09-13 02:50:16 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  experimental: {
+    nextScriptWorkers: true,
+  },
 }
-
-module.exports = nextConfig
